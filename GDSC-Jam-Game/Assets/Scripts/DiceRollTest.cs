e6d1f433ac4d26ee08c0f2df739ed823b20e5c0f
MightyBucky <i.n.dost38@gmail.com>
2023-02-01 23:18:32 +0300

    dice roll fixed

--- GDSC-Jam-Game/Assets/Scripts/DiceRollTest.cs
+++ GDSC-Jam-Game/Assets/Scripts/DiceRollTest.cs
@@ -2,25 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DiceRollTest : MonoBehaviour
 {
-    private DiceRoll[] _dices;
+    [SerializeField] private DiceRoll[] _dices;
     [SerializeField] private TMP_Text diceTotal;
 
-    private void Awake() 
-    {
-        _dices = GetComponentsInChildren<DiceRoll>();
-    }
-    
-    private void Start() 
-    {
-        SetDicesActive(false);
-    }
 
-    private int Roll(){
-        diceTotal.gameObject.SetActive(true);
-        SetDicesActive(true);
+    public void Roll(){
+        diceTotal.gameObject.SetActive(false);
 
         int sum = 0;
         foreach(DiceRoll dice in _dices){
@@ -29,20 +21,14 @@ public class DiceRollTest : MonoBehaviour
         //totalvalue animasyondan önce display ediliyor
         diceTotal.text = sum.ToString();
 
-        return sum;
-    }
-
-    private void SetDicesActive(bool flag)
-    {
-        foreach(var dice in _dices) 
-        {
-            dice.gameObject.SetActive(flag);
-        }
+        StartCoroutine(Wait());
 
     }
-    
-    public void Trigger() 
-    {
-        this.Roll();
+
+    IEnumerator Wait(){
+        EventSystem.current.currentSelectedGameObject.GetComponent<Button>().interactable = false;
+        yield return new WaitForSecondsRealtime(2f);
+        diceTotal.gameObject.SetActive(true);
+
     }
 }
